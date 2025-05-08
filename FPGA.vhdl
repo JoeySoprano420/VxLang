fd6228811be02fb03eb56490b03f8187270b6451
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-05-08 00:29:58 -0400

    Update FPGA.vhdl

--- FPGA.vhdl
+++ FPGA.vhdl
@@ -17,3 +17,23 @@ begin
     end if;
   end process;
 end Behavioral;
+
+      library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity FPGA_Label is
+  Port ( clk : in STD_LOGIC;
+         data_in : in STD_LOGIC_VECTOR(127 downto 0);
+         label_out : out STD_LOGIC_VECTOR(127 downto 0));
+end FPGA_Label;
+
+architecture Behavioral of FPGA_Label is
+begin
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      label_out <= data_in xor x"DEADBEEF";  -- FPGA-optimized label encoding
+    end if;
+  end process;
+end Behavioral;
